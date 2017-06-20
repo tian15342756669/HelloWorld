8ee292a330b35270206548d1ecccb70cf6af0443
邓天广 <dengtianguang@dengtianguangdeMacBook-Pro.local>
2017-06-20 10:50:36 +0800

    Initial commit

---
+++ src/com/hispeed/mobile/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.hispeed.mobile.controller;
+
+/**
+ * Created by dengtianguang on 2017/6/15.
+ */
+public class UserController {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!");
+    }
+}
